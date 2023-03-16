65517f64486c0d913b5b64ae9de529efab2ad281
Kyle Garner <garnerk10@gmail.com>
2023-03-15 22:03:28 -0400

    set layout of form

---
+++ library_app.js
@@ -0,0 +1,17 @@
+let myLibrary = [{
+    name: "A Game of Thrones",
+    author: "George R. R. Martin",
+    pages: 835,
+    read: false
+}];
+
+function Book(name, author, pages) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = true;
+};
+
+const addBookToLibrary = () => {
+
+}
