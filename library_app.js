91341bddbecfb83cbe719c9a3e8c797fc6d2d85c
Kyle Garner <garnerk10@gmail.com>
2023-03-16 10:58:21 -0400

    function to add to library

--- library_app.js
+++ library_app.js
@@ -2,16 +2,37 @@ let myLibrary = [{
     name: "A Game of Thrones",
     author: "George R. R. Martin",
     pages: 835,
-    read: false
+    read: false,
+    id: 1
 }];
 
-function Book(name, author, pages) {
-    this.name = name;
+function Book(title, author, pages) {
+    this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = true;
+    this.id = myLibrary.length + 1;
 };
 
-const addBookToLibrary = () => {
+function addBookToLibrary() {
+    document.getElementById("add_form").addEventListener("submit", function(event){
+        event.preventDefault();
+    });
 
+    myLibrary.push(new Book(
+        document.getElementById("title").value,
+        document.getElementById("author").value,
+        document.getElementById("pages").value,
+        document.getElementsByName("read").value
+    ));
+
+    document.getElementById("popup_form").style.display = "none";
+    document.getElementById("new_button").style.display = "flex";
+
+    console.log(myLibrary)
 }
+
+const show_form = () => {
+   document.getElementById("popup_form").style.display = "flex";
+   document.getElementById("new_button").style.display = "none";
+};
